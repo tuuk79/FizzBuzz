5ab678dd3e14f0ec621c9f060f90e7141a946948
Steve Yoo <stevecyoo@gmail.com>
2024-05-30 18:46:48 -0500

    tested all inputs

--- src/fizzbuzz.service.test.ts
+++ src/fizzbuzz.service.test.ts
@@ -5,10 +5,52 @@ describe('fizzbuzz service', () => {
         expect(fizzBuzz).not.toBeUndefined();
     });
 
-    it('should return "Divided 1 by 3" when input is 1', () => {
-        const input = 1;
+    it('should return ["Divided 1 by 3","Divided 1 by 5"] when input is 1', () => {
+        const input = '1';
         const actual = fizzBuzz(input);
-        const expected = 'Divided 1 by 3';
-        expect(actual).toBe(expected);
+        const expected = ['Divided 1 by 3', 'Divided 1 by 5'];
+        expect(actual).toEqual(expected);
+    });
+
+    it('should return ["Fizz"] when input is 3', () => {
+        const input = '3';
+        const actual = fizzBuzz(input);
+        const expected = ['Fizz'];
+        expect(actual).toEqual(expected);
+    });
+
+    it('should return ["Buzz"] when input is 5', () => {
+        const input = '5';
+        const actual = fizzBuzz(input);
+        const expected = ['Buzz'];
+        expect(actual).toEqual(expected);
+    });
+
+    it('should return ["Invalid Item"] when input is an empty string', () => {
+        const input = '';
+        const actual = fizzBuzz(input);
+        const expected = ['Invalid Item'];
+        expect(actual).toEqual(expected);
+    });
+
+    it('should return ["FizzBuzz"] when input is 15', () => {
+        const input = '15';
+        const actual = fizzBuzz(input);
+        const expected = ['FizzBuzz'];
+        expect(actual).toEqual(expected);
+    });
+
+    it('should return ["Invalid Item"] when input is A', () => {
+        const input = 'A';
+        const actual = fizzBuzz(input);
+        const expected = ['Invalid Item'];
+        expect(actual).toEqual(expected);
+    });
+
+    it('should return ["Divided 23 by 3","Divided 23 by 5"] when input is 23', () => {
+        const input = '23';
+        const actual = fizzBuzz(input);
+        const expected = ["Divided 23 by 3", "Divided 23 by 5"];
+        expect(actual).toEqual(expected);
     });
 });
--- src/fizzbuzz.service.ts
+++ src/fizzbuzz.service.ts
@@ -1,8 +1,9 @@
-const fizzBuzz = (input: number): string => {
-    if (input === 1) {
-        return 'Divided 1 by 3';
-    }
-    return '';
+const fizzBuzz = (input: string): string[] => {
+    if (!input || Number.isNaN(Number(input))) return ['Invalid Item'];
+    if (Number(input) % 3 === 0 && Number(input) % 5 === 0) return ['FizzBuzz'];
+    if (Number(input) % 3 === 0) return ['Fizz'];
+    if (Number(input) % 5 === 0) return ['Buzz'];
+    return [`Divided ${input} by 3`, `Divided ${input} by 5`];
 };
 
 export { fizzBuzz };
